8e2dd93af494c74d141cca2b2786a02fe70b815a
Nishikino <2376593337@qq.com>
2021-09-10 10:14:31 +0800

    访问静态资源的一些配置

--- backend/tdr/src/main/java/com/xttdr/common/config/WebConfig.java
+++ backend/tdr/src/main/java/com/xttdr/common/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.xttdr.common.AuthInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,13 +12,20 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+
         registry.addInterceptor(authInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/login", "/register", "/imserver/**", "/files/**");
+                .excludePathPatterns("/login", "/register", "/imserver/**", "/files/**", "/static/**");
     }
 
     @Bean
     public AuthInterceptor authInterceptor() {
         return new AuthInterceptor();
+    }
+
+    /*在这里添加访问静态资源的路径*/
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**").addResourceLocations("classpath:/files/");
     }
 }
